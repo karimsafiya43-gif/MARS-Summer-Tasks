8221f274d69e650e35c29491a2f31d95e472f151
karimsafiya43-gif <karimsafiya43@gmail.com>
2026-06-17 17:05:59 +0530

    Add files via upload

---
+++ task4/launch.py
@@ -0,0 +1,121 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, TimerAction
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    pkg = get_package_share_directory('rover')
+
+    xacro_file = os.path.join(pkg, 'urdf', 'robot.urdf.xacro')
+    robot_description = xacro.process_file(xacro_file).toxml()
+    
+    ekf_config_path = os.path.join(pkg, 'config', 'ekf.yaml')
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': True
+        }]
+    )
+
+    gazebo = ExecuteProcess(
+    cmd=['ign', 'gazebo', '-r',
+         '/home/safiya/ros2_ws/src/rover/world/world.sdf'],
+    output='screen'
+)
+
+    spawn = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'shakti',
+            '-topic', 'robot_description',
+            '-x', '0', '-y', '0', '-z', '0.04'
+        ],
+        output='screen'
+    )
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/joint_states@sensor_msgs/msg/JointState[ignition.msgs.Model',
+            '/clock@rosgraph_msgs/msg/Clock@ignition.msgs.Clock',
+            '/camera/image_raw@sensor_msgs/msg/Image@ignition.msgs.Image',
+            '/camera/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo',
+            '/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
+            '/odom@nav_msgs/msg/Odometry@ignition.msgs.Odometry',
+            '/scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
+            '/imu@sensor_msgs/msg/Imu@ignition.msgs.IMU',
+        ],
+        parameters=[{'use_sim_time': True}],
+        output='screen'
+    )
+
+   
+    joint_state_broadcaster = TimerAction(
+        period=5.0,
+        actions=[Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=[
+                'joint_state_broadcaster',
+                '--controller-manager', '/controller_manager'
+            ],
+        )]
+    )
+
+    arm_controller = TimerAction(
+        period=7.0,
+        actions=[Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=[
+                'arm_controller',
+                '--controller-manager', '/controller_manager'
+            ],
+        )]
+    )
+    
+    lidar_tf = Node(
+  		package='tf2_ros',
+    		executable='static_transform_publisher',
+    		arguments=[
+       			'0', '0', '0',
+       			'0', '0', '0',
+       			'base_l',
+       			'shakti/base_l/gpu_lidar'
+			]
+			)
+			
+			
+    tf_bridge = Node(
+		    package='ros_gz_bridge',
+		    executable='parameter_bridge',
+		    arguments=[
+			'/model/shakti/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V',
+		    ],
+		    remappings=[
+			('/model/shakti/tf', '/tf'),
+		    ],
+		    parameters=[{'use_sim_time': True}],
+		    output='screen'
+		)
+							
+   
+		
+
+    return LaunchDescription([
+        gazebo,
+        bridge,
+        tf_bridge,
+        robot_state_publisher,
+        spawn,
+        joint_state_broadcaster,
+        arm_controller,
+        lidar_tf
+    ])
